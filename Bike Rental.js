922396304d996854a6130ce740599398d3de82fd
SonaliGusain <150153360+SonaliGusain@users.noreply.github.com>
2025-03-19 12:46:14 +0530

    Delete Bike Rental.js

--- Bike Rental.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const rentButton = document.querySelector(".rental-box button");
-
-    rentButton.addEventListener("click", function () {
-        alert("Your bike or scooty has been booked successfully!");
-    });
-});
