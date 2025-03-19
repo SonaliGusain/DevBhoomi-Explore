de3ae82393767b11496763ec460d699b9d139254
SonaliGusain <150153360+SonaliGusain@users.noreply.github.com>
2025-03-19 12:47:31 +0530

    Delete car rental.js

--- car rental.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const searchButton = document.querySelector(".search-box button");
-
-    searchButton.addEventListener("click", function () {
-        alert("Searching for available cars...");
-    });
-});
