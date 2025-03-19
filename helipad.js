3df5848f78f027f7b6019352ec8f1a403721dfc8
SonaliGusain <150153360+SonaliGusain@users.noreply.github.com>
2025-03-19 12:48:31 +0530

    Delete helipad.js

--- helipad.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const bookButton = document.querySelector(".booking-box button");
-
-    bookButton.addEventListener("click", function () {
-        alert("Your helipad landing has been booked successfully!");
-    });
-});
-
